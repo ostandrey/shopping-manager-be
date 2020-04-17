10387655f08c61b6ac1411599b7bf876e7c096f5
Andrew Ostapov <ost.and.g@gmail.com>
2020-04-18 01:11:42 +0300

    Start back-end. New logic to add wallets.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { WalletModule } from './wallets/wallet-module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [WalletModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/wallets/wallet-controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { WalletService } from './service/wallet-service';
+import { WalletModel } from './models/wallet-model';
+
+
+@Controller('dashboard')
+export class WalletController {
+
+  constructor(private walletService: WalletService) {
+  }
+
+  @Get()
+  findAll(): Observable<WalletModel[]>{
+    return this.walletService.findAll();
+  }
+}
